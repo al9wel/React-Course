88456ad71ddee6e575808bf201e7d04af4dc4689
Salem Alswil <sa.al9wel@gmail.com>
2025-04-09 21:31:11 +0300

    array_state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,74 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import { useState } from "react";
+let key = 0;
 function App() {
+  const [inputValue, setInputValue] = useState("")
+  const [items, setItems] = useState([])
+  const handelInput = (e) => {
+    setInputValue(e.target.value)
+  }
+  const handleAddButton = () => {
+    if (inputValue !== "") {
+      let newObj = {
+        id: key,
+        title: inputValue
+      }
+      ++key;
+      setItems([...items, newObj])
+      setInputValue("")
+    }
+  }
+  const handelDelete = (id) => {
+    const delItem = [...items]
+    let i = 0;
+    let index = 0;
+    delItem.forEach((item) => {
+      if (item.id === id) {
+        index = i
+      }
+      i++;
+    })
+    delItem.splice(index, 1)
+    setItems(delItem)
+  }
+  const handelUpdate = (id) => {
+    if (inputValue !== "") {
+      let updItem = [...items]
+      let i = 0;
+      let index = 0;
+      updItem.forEach((item) => {
+        if (item.id === id) {
+          index = i;
+        }
+        i++;
+      })
+      updItem[index].title = inputValue;
+      setItems(updItem);
+      setInputValue("")
+    }
+  }
+  let itemsList = items.map((item) => {
+    return (
+      <div key={item.id} className="card">
+        <h2>{item.title}</h2>
+        <div>
+          <button onClick={() => { handelDelete(item.id) }}>DELETE</button>
+          <button onClick={() => { handelUpdate(item.id) }}>UPDATE</button>
+        </div>
+      </div>
+    )
+  })
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
+        <div>
+          <div className="card" style={{ backgroundColor: "#b71c1c47" }}>
+            <h3 style={{ margin: 0 }}>Add Task</h3>
+            <input placeholder="Add Or Update" onChange={handelInput} value={inputValue} type="text" />
+            <button onClick={handleAddButton}>ADD</button>
+          </div>
+          {itemsList}
+        </div>
       </header>
     </div>
   );
