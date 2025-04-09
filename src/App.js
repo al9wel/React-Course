92486ceccd4f17cc07991ec19ca830dedc57f17a
Salem Alswil <sa.al9wel@gmail.com>
2025-04-09 18:33:54 +0300

    props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from "./logo.svg";
-import "./App.css";
+
+import Post from "./Post";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* يتم ارسال البروبس عبر الاتربيوت اي كوستم اتربيوت تعمله يتم ارساله ك بروبس */}
+      <Post title="Salem" body="hi" content="salem alswil" />
+      <Post body="hi" />
+      <Post title="salem" />
+      {/*  ارسال ك بروبس jsx */}
+      <Post>
+        <button>hi</button>
+        <button>hello</button>
+      </Post>
+      <Post>
+        <Post title="Salem" body="hi" content="salem alswil" />
+      </Post>
+    </>
   );
 }
 export default App;
---
+++ src/Post.js
@@ -0,0 +1,18 @@
+// يتم استقبال البروبس هنا ك بارمتر واحد فقط
+// يعني لو ارسلت من هناك 10 بروبس سيتم استقبالها هنا ك اوبجكت
+
+// اوبجكت ديستركشر
+// هو تستخرج الاشياء اللي داخل الاوبكت ك متغيرات من البارمتر على طول
+export default function Post({ title, body, children = "no content" }) {
+    return (
+        <>
+            <div style={{ border: "2px solid #607D8B", padding: "15px", textAlign: "center", marginBottom: "10px", width: "70%", margin: "10px auto" }}>
+                <h1>{title != null ? title : "Post Title"}</h1>
+                <hr />
+                <h3>{body != null ? body : "Post body"}</h3>
+                <hr />
+                <div>{children}</div>
+            </div>
+        </>
+    )
+}
