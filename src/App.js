cffefef6d13a0ea674b26ecda4466e8519ab4305
Salem Alswil <sa.al9wel@gmail.com>
2025-10-07 18:03:36 +0300

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useMemo, useState } from "react";
 function App() {
+  const [value, setValue] = useState("")
+  const [number, setNumber] = useState(0)
+  // هاذه العمليه سيتم حسابها في كل مره يتم عمل ريرندر
+  const noUseMemo = number + 1
+  // هاذي العمليه سيتم حسابها في اول ريرندر فقط
+  const withUseMemo = useMemo(() => {
+    return number + 1
+  }, [])
+  const handleChange = (e) => {
+    setValue(e.target.value)
+    setNumber(number + 1)
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
+        <input value={value} onChange={handleChange} type="text" />
+        <h1>{noUseMemo}</h1>
+        <h1>{withUseMemo}</h1>
       </header>
     </div>
   );
